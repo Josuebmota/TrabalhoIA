b4d744065723784f2b03d425afccd5ffa804b3d2
Josué Batista <josuebatistam1@gmail.com>
2019-06-10 13:23:39 -0300

    Update No.py

--- No.py
+++ No.py
@@ -16,7 +16,12 @@ class No:
         return self.estado
     def setEstado(self, estado):
         self.estado = estado
-    
+
+    def getCusto(self):
+        return self.custo_do_caminho
+    def setCusto(self, custo_do_caminho):
+        self.custo_do_caminho = custo_do_caminho
+        
     def getProfundidade(self):
         return self.profundidade
     def setProfundidade(self, profundidade):
