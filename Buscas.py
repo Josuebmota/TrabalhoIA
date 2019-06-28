cbe7e6f4f3b8200889da48f59ac8ff821fc40249
Josué Batista <josuebatistam1@gmail.com>
2019-06-28 18:19:26 -0300

    Update Buscas.py

--- Buscas.py
+++ Buscas.py
@@ -38,13 +38,13 @@ class Buscas:
         while(True):
             Buscas.matrizprint(self,borda[0].estado)
             if(problema.teste_objetivo(borda[0]) == True ):#Atingir o objetivo
-                break
+                print("Profundidade Total:", borda[0].profundidade)
+                return Buscas.caminhos(self,borda[0])
             filhos = Buscas.expande(self,borda[0], problema) #Expandir
             random.shuffle(filhos) #Bagunçar a lista de filhos
             borda.pop(0)
             for acoes in range(len(filhos)):
                  borda.insert(0,filhos[acoes]) #Inserir no começo
-        return Buscas.caminhos(self,borda[0])
     
     def busca_em_largura(self, problema):
         borda = [] #Lista de Nos
@@ -54,35 +54,40 @@ class Buscas:
         while(True):
             Buscas.matrizprint(self,borda[0].estado)
             if(problema.teste_objetivo(borda[0]) == True ):#Atingir o objetivo
-                break
+                print("Profundidade Total:", borda[0].profundidade)
+                return Buscas.caminhos(self,borda[0])
             filhos = Buscas.expande(self,borda[0], problema) #Expandir
             #random.shuffle(filhos) #Bagunçar a lista de filhos
             borda.pop(0)
             for acoes in range(len(filhos)):
                  borda.append(filhos[acoes])#Inserir no final
-        return Buscas.caminhos(self,borda[0])
+       
 
     def busca_em_profundidade_limitada(self, problema,limite):
         borda = []
         Nos = No()
         Nos._init_(problema.estado_inicial,None, 0, 0)
         borda.insert(0,Nos)
-        while(borda[0].profundidade!=limite+1):
+        while(True):
             Buscas.matrizprint(self,borda[0].estado)
-            if(problema.teste_objetivo(borda[0]) == True ):
-                return True 
-            filhos = Buscas.expande(self,borda[0], problema)
-            random.shuffle(filhos)
+            if(borda==[]):
+                print("Profundidade Total:", borda[0].profundidade)
+                return Buscas.caminhos(self,borda[0])
+            if(problema.teste_objetivo==borda[0]):
+                print("Profundidade Total:", borda[0].profundidade)
+                return Buscas.caminhos(self,borda[0])
+            if(borda[0].profundidade!=limite+1):
+                filhos = Buscas.expande(self,borda[0], problema)
+                random.shuffle(filhos)
+                for acoes in range(len(filhos)):
+                    borda.insert(0,filhos[acoes])
             borda.pop(0)
-            for acoes in range(len(filhos)):
-                 borda.insert(0,filhos[acoes])
-           
-        print("Nao atingiu o objetivo neste limite")
-        return False
     
     def busca_em_aprofundamento_interativo(self, problema, interacao):
-        for i in range(interacao+1):
-            resultado = Buscas.busca_em_profundidade_limitada(self,problema, i)
+        interacao = 0
+        while(True):
+            resultado = Buscas.busca_em_profundidade_limitada(self,problema, interacao)
+            interacao+=1
             if(resultado == True):
                 break
             print("-----------------------------------------------")
@@ -95,22 +100,21 @@ class Buscas:
         Nos._init_(problema.estado_inicial,None, 0, 0) #No inicial
         borda.insert(0,Nos) #Inserindo
         while(True):
-            Buscas.matrizprint(self,borda[0].estado)
+            Buscas.matrizprint(self,borda[0].estado) 
             if(borda==[]):
-                print("Deu ruim")
-                return visitado
+                print("Profundidade Total:", borda[0].profundidade)
+                return Buscas.caminhos(self,borda[0])
             if(problema.teste_objetivo(borda[0]) == True ):#Atingir o objetivo
-                return visitado
+                print("Profundidade Total:", borda[0].profundidade)
+                return Buscas.caminhos(self,borda[0])
+            while(True):
+                if not borda[0] in visitado:
+                    visitado.append(borda[0])
+                    break
+                borda.pop(0)
             filhos = Buscas.expande(self,borda[0], problema) #Expandir
             random.shuffle(filhos) #Bagunçar a lista de filhos
-            if not borda[0] in visitado:
-                visitado.append(borda[0])
             borda.pop(0)
-            for i in range(len(visitado)):
-                aux = visitado[i]
-                for j in range(len(filhos)):
-                    if(aux==filhos[j]):
-                        del(filhos[j])
             for acoes in range(len(filhos)):
                  borda.insert(0,filhos[acoes]) #Inserir no começo
     
@@ -122,12 +126,10 @@ class Buscas:
         while(True):
             Buscas.matrizprint(self,borda[0].estado)
             if(problema.teste_objetivo(borda[0]) == True ):#Atingir o objetivo
-                print(borda[0].custo_do_caminho)
-                break
+                print("Profundidade Total:", borda[0].profundidade)
+                return Buscas.caminhos(self,borda[0])
             filhos = Buscas.expande(self,borda[0], problema) #Expandir
             borda.sort(key=attrgetter("custo_do_caminho"))
             borda.pop(0)
             for acoes in range(len(filhos)):
                  borda.append(filhos[acoes])#Inserir no final
-                 
-        return Buscas.caminhos(self,borda[0])
