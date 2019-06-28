08ff39347a6814f2bf7f222cf67342d795c38171
Josué Batista <josuebatistam1@gmail.com>
2019-06-28 18:19:29 -0300

    Update Puzzle8.py

--- Puzzle8.py
+++ Puzzle8.py
@@ -5,15 +5,24 @@ import copy
 import time
 
 class Puzzle8:
+    
+    def estado_inicial(self):
+        problema = Problemas()
+        problema._init_([[0, 1, 2], [3, 4, 5], [6, 7, 8]], self.acao, self.teste_inicial,1)
+        resultado = Buscas.busca_em_profundidade_limitada(self,problema, 20) 
+        print(resultado[0].estado)
+        return resultado[0].estado
 
-    tabuleiro = [[7,2,4],[5,0,6],[8,3,1]]
-
-    def teste_objetivo(self, no):
-        objetivo = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
-        if(no.estado == objetivo):
+    def teste_inicial(self, no):
+        if(no.estado ==  [[0, 0, 0], [0, 0, 0], [0, 0, 0]]):
             return True
         return False
-    
+
+    def teste_objetivo(self, no):
+        if(no.estado ==  [[0, 1, 2], [3, 4, 5], [6, 7, 8]]):
+            return True
+        return False
+
     def acao(self, no):
         l= 0
         c= 0 
@@ -24,9 +33,9 @@ class Puzzle8:
                 if(puzzle[i][j]==0):
                     l=i
                     c=j
-        
+
         #mover para direita
-        if(c!=2):
+        if(c!=2):   
             aux = copy.deepcopy(puzzle)
             aux[l][c] = puzzle[l][c+1]
             aux[l][c+1] = puzzle[l][c]
@@ -49,13 +58,12 @@ class Puzzle8:
             aux[l][c] = puzzle[l+1][c]
             aux[l+1][c] = puzzle[l][c]
             vetor.append(aux)
-        
         return vetor 
 
     def main(self):
         ini = time.time()
         problema = Problemas()
-        problema._init_(self.tabuleiro, self.acao, self.teste_objetivo, 1)
+        problema._init_(self.estado_inicial(), self.acao, self.teste_objetivo, 1)
         Buscas.busca_em_largura(self,problema)
         fim = time.time()
         print("Time:", fim - ini)
